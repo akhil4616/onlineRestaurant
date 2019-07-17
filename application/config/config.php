036423a1d28ed587e40b76a51be73aee4670df6b
akhil4616 <51798314+akhil4616@users.noreply.github.com>
2019-07-17 22:08:24 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/restaurant';
+$config['base_url'] = 'http://localhost/onlineRestaurant';
 
 $config['index_page'] = 'index.php';
 
